273d48bd75b59d2ee0f44089646ff0de1b97da1c
Hồng Lê Đăng Khoa <hongkhoa348@gmail.com>
2025-10-15 23:43:23 +0700

    feat: modified property card
    add image and modified property page

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,4 +1,20 @@
+import properties from "../../properties.json";
+import PropertyCard from "@/components/PropertyCard";
 const PropertiesPage = () => {
-  return <div>Khoa Hồng học react</div>;
+  return (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:conatiner m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <p>No properties available.</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((properties) => (
+              <PropertyCard key={properties._id} property={properties} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 export default PropertiesPage;
--- components/InforBox.tsx
+++ components/InforBox.tsx
@@ -1,5 +1,38 @@
-const InforBox = () => {
-  return <div>InforBox</div>;
+import Link from "next/link";
+
+interface ButtonInfo {
+  link: string;
+  text: string;
+  backgroundColor: string;
+}
+
+interface InforBoxProps {
+  children: string;
+  heading: string;
+  backgroundColor?: string;
+  textColor?: string;
+  buttonInfo: ButtonInfo;
+}
+
+const InforBox = ({
+  children,
+  heading,
+  backgroundColor = "bg-gray-100",
+  textColor = "text-gray-700",
+  buttonInfo,
+}: InforBoxProps) => {
+  return (
+    <div className={`${backgroundColor} p-6 rounded-lg shadow-md`}>
+      <h2 className={`${textColor} text-2xl font-bold`}>{heading}</h2>
+      <p className={`${textColor} mt-2 mb-4`}>{children}</p>
+      <Link
+        href={buttonInfo.link}
+        className={`inline-block ${buttonInfo.backgroundColor} text-white rounded-lg px-4 py-2 hover:bg-gray-700`}
+      >
+        {buttonInfo.text}
+      </Link>
+    </div>
+  );
 };
 
 export default InforBox;
